e7d3cf3de5d0e40f7c3012bbe6d7394bdbeb4125
thierryderuyttere <thierryderuyttere@hotmail.com>
2016-02-22 19:27:50 +0100

    x-frame

--- appy_server/views.py
+++ appy_server/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, redirect
 from django.utils.translation import ugettext as _
 from django.contrib.auth.hashers import make_password
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.decorators.clickjacking import xframe_options_exempt
 
+@xframe_options_exempt
 def home(request):
     return render(request, 'output.html')
