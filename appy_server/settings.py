e90f064fdceb9083c6fa5fd5943522031dfa9dfa
thierryderuyttere <thierryderuyttere@hotmail.com>
2016-06-18 14:48:46 +0200

    test

--- appy_server/settings.py
+++ appy_server/settings.py
@@ -27,8 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -109,3 +107,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = '/static/'
